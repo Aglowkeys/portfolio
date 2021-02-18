6bc9243b92b8a8a58af2aac2813326155a0b939d
Aglowkeys <emilianoalfonso@outlook.com>
2021-02-18 12:01:09 -0300

    a

--- src/components/Name.js
+++ src/components/Name.js
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 
 const StyledName = styled.h1`
 	font-size: var(--h-lg);
-	font-size: clamp(2.5rem, 10vw, var(--h-lg));
+	/* font-size: clamp(2.5rem, 10vw, var(--h-lg)); */
 	color: var(--primary-200);
 	line-height: 1;
 	letter-spacing: -0.075em;
-	padding-right: 0.1em; // Porque la font se me corta, creo que por cómo está hecha
+	max-width: 8ch;
 
 	@media (max-width: 1000px) {
 		font-size: var(--h-md);
 	}
+
 	@supports (background-clip: text) or (-webkit-background-clip: text) {
 		color: transparent;
 		background-image: linear-gradient(
@@ -25,14 +26,6 @@ const StyledName = styled.h1`
 			var(--primary-200),
 			var(--secondary)
 		); // Para arreglar bug con Safari. Parece que no le gustan las etiquetas dentro de h1
-	}
-
-	// Para evitar bugs en Safari
-	/* 	@supports (font-size: clamp(2.5rem, 10vw, var(--h-lg))) {
-	}
- */
-	span {
-		display: block;
 	}
 `;
 
