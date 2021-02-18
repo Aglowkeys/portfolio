c3bbe149d8da2916eede48ece419e3e87ff66684
Aglowkeys <emilianoalfonso@outlook.com>
2021-02-17 23:26:03 -0300

    Made the name validation string more user friendly

--- src/components/strings/Contact.js
+++ src/components/strings/Contact.js
@@ -17,7 +17,8 @@ export default {
 			'There was an error trying to send your message. Please try again.',
 		errorNameIncomplete: 'Please enter your name.',
 		errorNameShort: 'Your name must be at least 3 characters long.',
-		errorNameInvalid: 'The provided name is invalid.',
+		errorNameInvalid:
+			"The provided name is invalid. It may only contain letters and the following characters: - , . '",
 		errorMessage: 'Please enter a message.',
 		errorEmailIncomplete: 'Please enter an e-mail address.',
 		errorEmailInvalid: 'The provided e-mail is invalid.',
@@ -40,7 +41,8 @@ export default {
 			'Ocurrió un error al enviar tu mensaje. Por favor, inténtalo nuevamente.',
 		errorNameIncomplete: 'Por favor, ingresa tu nombre.',
 		errorNameShort: 'Tu nombre debe tener al menos 3 caracteres de largo.',
-		errorNameInvalid: 'El nombre ingresado es inválido.',
+		errorNameInvalid:
+			"El nombre ingresado es inválido. Solo se permiten letras y los siguientes símbolos: - , . '",
 		errorMessage: 'Por favor, escribe un mensaje.',
 		errorEmailIncomplete:
 			'Por favor, ingresa una dirección de correo electrónico.',
