bd7353a7faa563f64fe6c4ce939fa8ef2589f6ac
Aglowkeys <emilianoalfonso@outlook.com>
2021-02-18 01:39:21 -0300

    Removed clamp

--- src/components/Name.js
+++ src/components/Name.js
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 
 const StyledName = styled.h1`
 	font-size: var(--h-lg);
-	font-size: clamp(2.5rem, 10vw, var(--h-lg));
 	color: var(--primary-200);
 	line-height: 1;
 	letter-spacing: -0.075em;
 	padding-right: 0.1em; // Porque la font se me corta, creo que por cómo está hecha
 
+	@media (max-width: 1000px) {
+		font-size: var(--h-md);
+	}
 	@supports (background-clip: text) or (-webkit-background-clip: text) {
 		color: transparent;
 		background-image: linear-gradient(
@@ -18,6 +20,12 @@ const StyledName = styled.h1`
 		background-clip: text;
 		-webkit-background-clip: text;
 	}
+
+	// Para evitar bugs en Safari
+	/* 	@supports (font-size: clamp(2.5rem, 10vw, var(--h-lg))) {
+		font-size: clamp(2.5rem, 10vw, var(--h-lg));
+	}
+ */
 	span {
 		display: block;
 	}
