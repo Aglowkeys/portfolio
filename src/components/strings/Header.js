b0ca1b9c69d0d9be4e5ac604d252074b711df34d
Emiliano <70929618+Aglowkeys@users.noreply.github.com>
2021-04-14 01:16:06 -0300

    Update Header.js

--- src/components/strings/Header.js
+++ src/components/strings/Header.js
@@ -1,13 +1,13 @@
 export default {
 	en: {
 		description:
-			'I am a web designer who focuses on the small details, and on simple, accessible and adaptable designs.',
+			'I am a web developer who focuses on the small details, and on simple, accessible and adaptable designs.',
 		downloadCV: 'Download Resume',
 		cvLink: 'Emiliano_Alfonso_en.pdf',
 	},
 	es: {
 		description:
-			'Soy un diseñador web que hace foco en el detalle, los diseños simples, accesibles y adaptables.',
+			'Soy un desarrollador web que hace foco en el detalle, los diseños simples, accesibles y adaptables.',
 		downloadCV: 'Descargar CV',
 		cvLink: 'Emiliano_Alfonso_es.pdf',
 	},
