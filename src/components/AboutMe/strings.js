a84f9d58c2584d1b796b619d9573d370c442bb6f
Emiliano <70929618+Aglowkeys@users.noreply.github.com>
2021-07-07 14:21:50 -0300

    Update strings.js

--- src/components/AboutMe/strings.js
+++ src/components/AboutMe/strings.js
@@ -2,13 +2,13 @@ export default {
 	en: {
 		aboutMe: 'About Me',
 		description:
-			'Freelance developer, music teacher, Front-end lover. I attended a bootcamp with more than 700 hours of intensive learning that prepared me to work as a Full-stack developer, creating projects that were both real and complex. Furthermore, the bootcamp greatly improved my interpersonal skills and gave me the opportunity of teaching to beginner students.',
+			'Fullstack developer, music teacher, Front-end lover. I attended a bootcamp with more than 700 hours of intensive learning that prepared me to work as a Full-stack developer, creating projects that were both real and complex. Furthermore, the bootcamp greatly improved my interpersonal skills and gave me the opportunity of teaching to beginner students.',
 		seeProjects: 'See my projects',
 	},
 	es: {
 		aboutMe: 'Sobre mí',
 		description:
-			'Desarrollador freelance, docente de música, amante del Front-end. Realicé un bootcamp de más de 700 hs. de estudio intensivo que me preparó para desempeñarme como desarrollador Fullstack, ideando proyectos reales y complejos, además de potenciar mis habilidades interpersonales y haberme permitido ser mentor de estudiantes principiantes.',
+			'Desarrollador fullstack, docente de música, amante del Front-end. Realicé un bootcamp de más de 700 hs. de estudio intensivo que me preparó para desempeñarme como desarrollador Fullstack, ideando proyectos reales y complejos, además de potenciar mis habilidades interpersonales y haberme permitido ser mentor de estudiantes principiantes.',
 		seeProjects: 'Ver mis proyectos',
 	},
 };
