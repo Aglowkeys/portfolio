1f54a72138c775d21723cb0dd51aded21d268eb9
Aglowkeys <emilianoalfonso@outlook.com>
2021-12-24 20:12:50 -0300

    Fix form accessibility bug

--- src/components/Contact/Form/Input.js
+++ src/components/Contact/Form/Input.js
@@ -2,30 +2,32 @@ import { useField } from 'formik';
 import Exclamation from '../../Exclamation';
 
 const Input = ({ label, ...props }) => {
-	const [field, meta] = useField(props);
+    const [field, meta] = useField(props);
+    const { id } = props;
+    const isInvalid = meta.touched && meta.error;
 
-	return (
-		<div className='input__container'>
-			<label htmlFor={props.id || props.name}>{label}</label>
-			<input
-				{...field}
-				{...props}
-				required={true}
-				autoComplete='off'
-				aria-required={true}
-				aria-invalid={meta.error ? true : false}
-				aria-describedby={meta.error ? meta.error : null}
-			/>
-			{meta.touched && meta.error ? (
-				<div className='input__error'>
-					<div className='error__svg-container'>
-						<Exclamation />
-					</div>
-					{meta.error}
-				</div>
-			) : null}
-		</div>
-	);
+    return (
+        <div className='input__container'>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <input
+                {...field}
+                {...props}
+                required
+                aria-required
+                autoComplete='off'
+                aria-invalid={isInvalid}
+                aria-describedby={isInvalid ? `error-${id}` : undefined}
+            />
+            {isInvalid ? (
+                <div className='input__error' id={`error-${id}`}>
+                    <div className='error__svg-container' aria-hidden='true'>
+                        <Exclamation />
+                    </div>
+                    {meta.error}
+                </div>
+            ) : null}
+        </div>
+    );
 };
 
 export default Input;
