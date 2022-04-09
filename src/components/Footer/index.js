8f8e27ae568d968fcafcc9d29f4ae0eba34478e9
Aglowkeys <emilianoalfonso@outlook.com>
2022-04-08 22:25:43 -0300

    Update footer formatting and fix styling bug

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,42 +6,42 @@ import Container from '../Container';
 import { Github, Linkedin } from '@icons-pack/react-simple-icons';
 
 const Footer = () => {
-    const { Lang } = useContext(LanguageContext);
-    const s = strings[Lang];
+  const { Lang } = useContext(LanguageContext);
+  const s = strings[Lang];
 
-    return (
-        <StyledFooter>
-            <Container>
-                <div className='footer__icons'>
-                    <ul>
-                        <li>
-                            <a
-                                href='https://github.com/Aglowkeys/'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                                aria-label={s.github}
-                            >
-                                <Github size={35} />
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href='https://www.linkedin.com/in/emiliano-alfonso/'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                                aria-label={s.linkedin}
-                            >
-                                <Linkedin size={35} />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <p className='footer__mail'>
-                    <a href='mailto:emilianoalfonso1989@gmail.com'>emilianoalfonso1989@gmail.com</a>
-                </p>
-            </Container>
-        </StyledFooter>
-    );
+  return (
+    <StyledFooter>
+      <Container>
+        <div className='footer__icons'>
+          <ul>
+            <li>
+              <a
+                href='https://github.com/Aglowkeys/'
+                target='_blank'
+                rel='noopener noreferrer'
+                aria-label={s.github}
+              >
+                <Github size={35} />
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://www.linkedin.com/in/emiliano-alfonso/'
+                target='_blank'
+                rel='noopener noreferrer'
+                aria-label={s.linkedin}
+              >
+                <Linkedin size={35} />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <p className='footer__mail'>
+          <a href='mailto:emilianoalfonso1989@gmail.com'>emilianoalfonso1989@gmail.com</a>
+        </p>
+      </Container>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,81 +1,85 @@
 import styled from 'styled-components';
 
 export const StyledFooter = styled.footer`
-	padding: 3em 0;
-	position: relative;
+  padding: 3em 0;
+  position: relative;
 
-	a {
-		color: currentColor;
-	}
+  a {
+    color: currentColor;
+  }
 
-	a:hover {
-		color: var(--primary-100);
-	}
+  a:hover {
+    color: var(--primary-100);
+  }
 
-	& > * {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
+  & > * {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-		@media (max-width: 700px) {
-			flex-direction: column;
-		}
-	}
+    @media (max-width: 700px) {
+      flex-direction: column;
+    }
+  }
 
-	&::before {
-		content: '';
-		height: 5px;
-		background-image: linear-gradient(45deg, var(--primary-100), var(--secondary));
-		position: absolute;
-		top: 0;
-		left: 0;
-		right: 0;
-	}
+  &::before {
+    content: '';
+    height: 5px;
+    background-image: linear-gradient(45deg, var(--primary-100), var(--secondary));
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
 
-	ul {
-		display: flex;
-		list-style: none;
-		@media (max-width: 700px) {
-			margin-bottom: 1em;
-		}
+  ul {
+    display: flex;
+    list-style: none;
+    @media (max-width: 700px) {
+      margin-bottom: 1em;
+    }
 
-		li:last-child {
-			margin-left: 2em;
-		}
+    li:last-child {
+      margin-left: 2em;
+    }
 
-		a:focus {
-			outline: 4px dotted var(--primary-100);
-			outline-offset: 0.5em;
-		}
-	}
+    a {
+      display: inline-block;
+    }
 
-	.footer__mail a {
-		text-decoration: none;
-		position: relative;
+    a:focus {
+      outline: 4px dotted var(--primary-100);
+      outline-offset: 0.5em;
+    }
+  }
 
-		&:hover,
-		&:focus {
-			outline: none;
-			color: var(--primary-100);
-		}
+  .footer__mail a {
+    text-decoration: none;
+    position: relative;
 
-		@media (max-width: 550px) {
-			font-size: var(--body-sm);
-		}
+    &:hover,
+    &:focus {
+      outline: none;
+      color: var(--primary-100);
+    }
 
-		&:hover::before,
-		&:focus::before {
-			content: '';
-			height: 5px;
-			background-image: linear-gradient(45deg, var(--primary-100), var(--secondary));
-			position: absolute;
-			bottom: -0.5em;
-			left: 0;
-			right: 0;
+    @media (max-width: 550px) {
+      font-size: var(--body-sm);
+    }
 
-			@media (max-width: 550px) {
-				height: 3px;
-			}
-		}
-	}
+    &:hover::before,
+    &:focus::before {
+      content: '';
+      height: 5px;
+      background-image: linear-gradient(45deg, var(--primary-100), var(--secondary));
+      position: absolute;
+      bottom: -0.5em;
+      left: 0;
+      right: 0;
+
+      @media (max-width: 550px) {
+        height: 3px;
+      }
+    }
+  }
 `;
