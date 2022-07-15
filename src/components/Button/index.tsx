f65b803a220cde560b8f3065ccdd0671845b731a
Aglowkeys <emilianoalfonso@outlook.com>
2022-07-14 21:15:41 -0300

    Type and improve Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import type { FunctionComponent, ReactNode } from 'react';
+import { StyledButton } from './styles';
+
+type Props = {
+  children: ReactNode;
+  type?: 'button' | 'submit' | 'reset';
+  className?: string;
+  disabled?: boolean;
+}
+
+const Button: FunctionComponent<Props> = ({ children, disabled = false, ...rest }) => {
+	return (
+		<StyledButton disabled={disabled} {...rest}>
+			{children}
+		</StyledButton>
+	);
+};
+
+export default Button;
