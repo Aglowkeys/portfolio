6bfaf2f11bebbd4b8812b1f1a48c05782f0bcd6b
Aglowkeys <emilianoalfonso@outlook.com>
2022-07-15 20:31:30 -0300

    Add aria-hidden to Success component

--- src/components/Success/index.tsx
+++ src/components/Success/index.tsx
@@ -6,6 +6,7 @@ const Success: FunctionComponent = () => (
 		fill='currentColor'
 		viewBox='0 0 20 20'
 		xmlns='http://www.w3.org/2000/svg'
+    aria-hidden='true'
 	>
 		<path
 			fillRule='evenodd'
