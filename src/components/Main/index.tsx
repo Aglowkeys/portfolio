100cde1c78ed31274e6967d32b8e6f869064a11e
Aglowkeys <emilianoalfonso@outlook.com>
2022-07-14 21:19:50 -0300

    Type Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,12 @@
+import type { FunctionComponent } from 'react';
 import AboutMe from '../AboutMe';
 import Header from '../Header';
 import Projects from '../Projects';
 import Skills from '../Skills';
-import Contact from '../Contact';
+// @ts-ignore: Hasta que le ponga tipos a Contact, para que TS no explote. TODO: Quitar esto
+import Contact from '../Contact'; 
 
-const Main = () => {
+const Main: FunctionComponent = () => {
 	return (
 		<main>
 			<Header />
