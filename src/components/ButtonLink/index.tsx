dc344527bcbf5ed01dbb7441ee510fcbd48098c1
Aglowkeys <emilianoalfonso@outlook.com>
2022-07-14 21:15:59 -0300

    Type and improve ButtonLink

---
+++ src/components/ButtonLink/index.tsx
@@ -0,0 +1,25 @@
+import type { FunctionComponent, ReactNode } from 'react';
+import { StyledLink } from './styles';
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+  href: string;
+  target?: '_blank' | 'self';
+  ariaLabel?: string;
+  disabled?: boolean;
+}
+
+const ButtonLink: FunctionComponent<Props> = ({ children, ariaLabel, ...rest }) => {
+	return (
+		<StyledLink
+			rel='noopener noreferrer'
+			aria-label={ariaLabel}
+      {...rest}
+		>
+			{children}
+		</StyledLink>
+	);
+};
+
+export default ButtonLink;
