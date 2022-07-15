8f9d4c46b88faf1846518cd4e0a8f077a66c99c8
Aglowkeys <emilianoalfonso@outlook.com>
2022-07-14 21:20:49 -0300

    Type Project

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,50 @@
+import type { FunctionComponent } from 'react';
+import { StyledProject } from './styles';
+import { Fade } from 'react-awesome-reveal';
+import ButtonLink from '../ButtonLink';
+
+type Props = {
+  title: string;
+  desc: string;
+  githubLink: string;
+  githubLinkText: string;
+  githubLinkLabel?: string;
+  websiteLink: string;
+  websiteLinkText: string;
+  websiteLinkLabel?: string;
+  imageSrc: string;
+}
+
+const Project: FunctionComponent<Props> = ({
+  title,
+  desc,
+  imageSrc,
+  websiteLink,
+  websiteLinkLabel,
+  websiteLinkText,
+  githubLink,
+  githubLinkLabel,
+  githubLinkText,
+}) => (
+  <StyledProject>
+    <Fade triggerOnce>
+      <div className='project__img-container'>
+        <img src={imageSrc} alt={title} />
+      </div>
+    </Fade>
+
+    <h3 className='project__title'>{title}</h3>
+    <p className='small project__desc'>{desc}</p>
+
+    <div className='project__buttons'>
+      <ButtonLink href={websiteLink} ariaLabel={websiteLinkLabel}>
+        {websiteLinkText}
+      </ButtonLink>
+      <ButtonLink href={githubLink} ariaLabel={githubLinkLabel}>
+        {githubLinkText}
+      </ButtonLink>
+    </div>
+  </StyledProject>
+);
+
+export default Project;
