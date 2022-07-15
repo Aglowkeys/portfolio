5f900488fde529971b33dc108d8271924daf2435
Aglowkeys <emilianoalfonso@outlook.com>
2022-07-15 20:30:58 -0300

    Delete unused types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,1 @@
 export type Language = 'es' | 'en';
-
-export type String = Record<Language, Record<string, string>>;
-
-export type PortfolioProjectStrings = {
-  description: string;
-  ariaWebsite: string;
-  ariaRepository: string;
-}
