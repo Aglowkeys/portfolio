be95b45992fdd02b84110e8d495416961c2856ce
Aglowkeys <emilianoalfonso@outlook.com>
2022-07-14 21:20:02 -0300

    Type Me

--- src/components/Me/index.js
+++
@@ -1,6 +0,0 @@
-import { StyledPhoto } from './styles';
-import myphoto from '../../assets/fotomia.jpg';
-
-const Me = () => <StyledPhoto src={myphoto} alt='Emiliano Alfonso' />;
-
-export default Me;
---
+++ src/components/Me/index.tsx
@@ -0,0 +1,13 @@
+import type { FunctionComponent } from 'react';
+import { StyledPhoto } from './styles';
+import myphoto from '../../assets/fotomia.jpg';
+
+const Me: FunctionComponent = () => (
+  <StyledPhoto
+    src={myphoto}
+    alt='Emiliano Alfonso'
+    aria-hidden='true'
+  />
+);
+
+export default Me;
