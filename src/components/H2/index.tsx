1cfa89c8bfeb4b986d70b7ca986c621fd97c3d2f
Aglowkeys <emilianoalfonso@outlook.com>
2022-07-14 21:19:09 -0300

    Type H2

---
+++ src/components/H2/index.tsx
@@ -0,0 +1,18 @@
+import type { FunctionComponent, ReactNode } from 'react';
+import { StyledH2 } from './styles';
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+  id?: string;
+}
+
+const H2: FunctionComponent<Props> = ({ children, ...rest }) => {
+    return (
+        <StyledH2 {...rest}>
+            {children}
+        </StyledH2>
+    );
+};
+
+export default H2;
